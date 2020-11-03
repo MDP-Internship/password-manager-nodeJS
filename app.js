cc800d25afa901fba52f6809a7bde0f3f2766046
Abdullah Oğuz <oguzabdullah39@gmail.com>
2020-11-03 13:25:12 +0300

    girilen değerler şifreleme yapıldı

--- app.js
+++ app.js
@@ -36,7 +36,7 @@ var argv = require("yargs")
       .options({
         name: {
           demand: true,
-          description: "adınızı gireceğiniz arguman",
+          description: "hesabın adını gireceğiniz arguman",
           alias: "n",
           type: "string",
         },
@@ -51,87 +51,19 @@ var argv = require("yargs")
   })
   .help("help").argv;
 storage.initSync();
-
-function createAccount(account, masterPassword) {
-  //önceki kayıtları al
-  /*  var accounts = storage.getItem("accounts");
-  //eğer önceki kayıt yoksa array oluştur
-  if (typeof accounts === "undefined") {
-    accounts = [];
-  } */
-  //getAccount
-
- var getAccountObject = getAccounts(masterPassword);
-
-  //account verilerini array içine kaydet
-  getAccountObject.push(account);
-
-  //setItem kalıcı olarak kaydet
-  /*  storage.setItem("accounts", accounts); */
-  // saveAcccount
-  saveAccount(getAccountObject,masterPassword)
-
-  return account;
-}
-
-function saveAccount(accounts,masterPassword) {
-  // encrypt
-  var encryptedAccount = crypto.AES.encrypt(
-    JSON.stringify(accounts),
-    masterPassword
-  );
-
-  //setİtem
-
-  var savedAccount = storage.setItem(encryptedAccount, masterPassword);
-  // return account
-  return savedAccount;
-}
-
-function getAccounts(masterPassword) {
-  // getItem
-  var encryptedAccount = storage.getItem("accounts");
-  var accounts = [];
-  // decrtypt
-
-  if (typeof encryptedAccount !== "undefined") {
-    var bytes = crypto.AES.decrypt(encryptedAccount, masterPassword);
-    accounts = JSON.parse(bytes.toString(crypto.enc.Utf8));
-
-  }
-  console.log(accounts);
-  // return account array
-  return accounts;
-}
-
-function matchAccount(accountName, masterPassword) {
-  //getItem ile verileri getirmek ()
-
-  var getAccountItem = getAccounts(masterPassword);
-  var matchedAccount;
-
-  // forEach ile kayıtları dolaşarak accountName bulmak
-  getAccountItem.forEach(function (account) {
-    if (account.name === accountName) {
-      matchedAccount = account;
-    }
-  });
-  //return
-  return matchedAccount;
-}
-
 var command = argv._[0];
 console.log(command);
 
 if (
-  (command === "create" &&
-    typeof argv.name !== "undefined" &&
-    argv.name.length > 0 &&
-    typeof argv.username !== "undefined" &&
-    argv.username.length > 0 &&
-    typeof argv.password !== "undefined" &&
-    argv.password.length > 0,
-  typeof argv.masterPassword !== "undefined" && argv.masterPassword.length > 0)
+  command === "create" &&
+  typeof argv.name !== "undefined" &&
+  argv.name.length > 0 &&
+  typeof argv.username !== "undefined" &&
+  argv.username.length > 0 &&
+  typeof argv.password !== "undefined" &&
+  argv.password.length > 0 &&
+  typeof argv.masterPassword !== "undefined" &&
+  argv.masterPassword.length > 0
 ) {
   createAccount(
     {
@@ -141,13 +73,17 @@ if (
     },
     argv.masterPassword
   );
+
   console.log("Hesap Oluşturuldu...");
 } else if (
   command === "get" &&
   typeof argv.name !== "undefined" &&
-  argv.name.length > 0,  typeof argv.masterPassword !== "undefined" && argv.masterPassword.length > 0
+  argv.name.length > 0 &&
+  typeof argv.masterPassword !== "undefined" &&
+  argv.masterPassword.length > 0
 ) {
   var account = matchAccount(argv.name, argv.masterPassword);
+  console.log(account);
   if (typeof account !== "undefined") {
     console.log(account);
   } else {
@@ -155,6 +91,63 @@ if (
   }
 } else {
   console.log("lütfen geçerli bir komut giriniz");
+}
+
+function createAccount(account, masterPassword) {
+  var getAccountObject = getAccounts(masterPassword);
+
+  //account verilerini array içine kaydet
+  getAccountObject.push(account);
+
+  saveAccount(getAccountObject, masterPassword);
+
+  return account;
+}
+
+function saveAccount(accounts, masterPassword) {
+  // encrypt
+  var encryptedAccount = crypto.AES.encrypt(
+    JSON.stringify(accounts),
+    masterPassword
+  );
+
+  //setİtem
+
+  storage.setItem("accounts", encryptedAccount.toString());
+  // return account
+  return accounts;
+}
+
+function getAccounts(masterPassword) {
+  // getItemSync accounts verisini cek..
+  var encryptedAccounts = storage.getItem("accounts");
+  console.log("enc" + encryptedAccounts);
+  var accounts = [];
+
+  // decrypt
+  if (typeof encryptedAccounts !== "undefined") {
+    var bytes = crypto.AES.decrypt(encryptedAccounts, masterPassword);
+    console.log(bytes);
+    accounts = JSON.parse(bytes.toString(crypto.enc.Utf8));
+  }
+
+  // return accounts array
+  return accounts;
+  // return
+}
+
+function matchAccount(accountName, masterPassword) {
+  var accounts = getAccounts(masterPassword);
+
+  var matchedAccount;
+
+  // forEach butun kayitlari dolasarak accountName bulunacak..
+  accounts.forEach(function (account) {
+    if (account.name === accountName) {
+      matchedAccount = account;
+    }
+  });
+  return matchedAccount;
 }
 
 /* createAccount({
